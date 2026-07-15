48d171a01f8917f76ade6d9bec4f9e99b7bc3842
Nane Cerqueira <elaine.edc@hotmail.com>
2026-07-15 13:04:43 -0300

    ajuste para inclusão da imagem

--- src/components/ResumoCompartilhavel.jsx
+++ src/components/ResumoCompartilhavel.jsx
@@ -1,6 +1,8 @@
 import { useState, useMemo, useRef } from 'react'
 import { toJpeg } from 'html-to-image'
 import { AdesivoEstrela } from './Adesivos'
+import * as htmlToImage from 'html-to-image';
+
 
 const MAX_DESTAQUES = 5
 
